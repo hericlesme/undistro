40acc96afb46fa9aafc25b268bd0bbe5b13bd520
Felipe Oliveira <fpo@felipeweb.dev>
2020-08-20 12:06:03 -0300

    stream the logs until cluster is ready in create cluster
    closes #6
    
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- client/cluster/log.go
+++ client/cluster/log.go
@@ -5,17 +5,17 @@ Copyright 2020 Getup Cloud. All rights reserved.
 package cluster
 
 import (
-	"bufio"
 	"context"
-	"fmt"
 	"io"
-	"strings"
 	"sync"
+	"time"
 
 	undistrov1 "github.com/getupcloud/undistro/api/v1alpha1"
 	"github.com/getupcloud/undistro/internal/scheme"
+	"github.com/getupcloud/undistro/log"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 )
@@ -25,71 +25,86 @@ const (
 )
 
 type LogStreamer interface {
-	Stream(context.Context, *rest.Config, chan<- string, types.NamespacedName) error
+	Stream(context.Context, *rest.Config, io.Writer, types.NamespacedName) error
 }
 
 type logStreamer struct {
-	client     *rest.RESTClient
+	client     *kubernetes.Clientset
 	kubeClient client.Client
 }
 
-func (l *logStreamer) Stream(ctx context.Context, c *rest.Config, logChan chan<- string, nm types.NamespacedName) error {
-	var err error
-	l.client, err = rest.UnversionedRESTClientFor(c)
-	if err != nil {
-		return err
-	}
+func (l *logStreamer) Stream(ctx context.Context, c *rest.Config, w io.Writer, nm types.NamespacedName) error {
+	var (
+		err    error
+		cancel context.CancelFunc
+	)
+	ctx, cancel = context.WithCancel(ctx)
 	l.kubeClient, err = client.New(c, client.Options{Scheme: scheme.Scheme})
 	if err != nil {
+		cancel()
+		return err
+	}
+	l.client, err = kubernetes.NewForConfig(c)
+	if err != nil {
+		cancel()
 		return err
 	}
 	var podList corev1.PodList
 	err = l.kubeClient.List(ctx, &podList, client.InNamespace(undistroNamespace))
 	if err != nil {
+		cancel()
 		return err
 	}
 	logs := l.getLogs(podList.Items)
-	return l.streamLogs(ctx, logs, nm, logChan)
+	cerr := make(chan error, 1)
+	go func(ctx context.Context, cancel context.CancelFunc) {
+		ready, err := l.clusterIsReady(ctx, nm)
+		if err != nil {
+			cerr <- err
+			return
+		}
+		for !ready {
+			ready, err = l.clusterIsReady(ctx, nm)
+			if err != nil {
+				cerr <- err
+				return
+			}
+			<-time.After(10 * time.Second)
+		}
+		cancel()
+	}(ctx, cancel)
+	go func(ctx context.Context) {
+		cerr <- l.streamLogs(ctx, logs, w)
+	}(ctx)
+	select {
+	case err = <-cerr:
+		return err
+	case <-ctx.Done():
+		return nil
+	}
 }
 
-func (l *logStreamer) streamLogs(ctx context.Context, reqs []logRequest, nm types.NamespacedName, logChan chan<- string) error {
+func (l *logStreamer) streamLogs(ctx context.Context, reqs []logRequest, writer io.Writer) error {
 	wg := &sync.WaitGroup{}
 	wg.Add(len(reqs))
 	for _, req := range reqs {
-		go l.readLog(ctx, wg, nm, req, logChan)
+		reader, err := req.req.Stream()
+		if err != nil {
+			return err
+		}
+		go l.readLog(ctx, reader, writer)
 	}
 	wg.Wait()
 	return nil
 }
 
-func (l *logStreamer) readLog(ctx context.Context, wg *sync.WaitGroup, nm types.NamespacedName, r logRequest, logChan chan<- string) {
-	reader, err := r.req.Stream()
-	if err != nil {
-		logChan <- fmt.Sprintf("%s - %v", r.podName, err)
+func (l *logStreamer) readLog(ctx context.Context, reader io.ReadCloser, writer io.Writer) {
+	_, err := io.Copy(writer, reader)
+	if err != nil && err != context.DeadlineExceeded {
+		log.Log.Error(err, "fail to copy")
 		return
 	}
 	defer reader.Close()
-	ready, err := l.clusterIsReady(ctx, nm)
-	if err != nil {
-		logChan <- fmt.Sprintf("%s - %v", r.podName, err)
-		return
-	}
-	bReader := bufio.NewReader(reader)
-	for !ready {
-		var str string
-		str, err = bReader.ReadString('\n')
-		if err != nil && err != io.EOF {
-			logChan <- fmt.Sprintf("%s - %v", r.podName, err)
-		}
-		if strings.Contains(str, nm.Name) {
-			logChan <- fmt.Sprintf("%s - %s", r.podName, str)
-		}
-		ready, err = l.clusterIsReady(ctx, nm)
-		if err != nil {
-			logChan <- fmt.Sprintf("%s - %v", r.podName, err)
-		}
-	}
-	close(logChan)
 }
 
 func (l *logStreamer) clusterIsReady(ctx context.Context, nm types.NamespacedName) (bool, error) {
@@ -104,15 +119,13 @@ func (l *logStreamer) clusterIsReady(ctx context.Context, nm types.NamespacedNam
 func (l *logStreamer) getLogs(pods []corev1.Pod) []logRequest {
 	reqs := make([]logRequest, len(pods))
 	for i, pod := range pods {
-		req := l.client.Get().
-			Name(pod.Name).
-			Namespace(undistroNamespace).
-			Resource("pods").
-			SubResource("log").
-			Param("follow", "true").
-			Param("timestamps", "true").
-			Param("previous", "false").
-			Param("container", "manager")
+		req := l.client.
+			CoreV1().
+			Pods(undistroNamespace).
+			GetLogs(pod.Name, &corev1.PodLogOptions{
+				Container: "manager",
+				Follow:    true,
+			})
 		reqs[i] = logRequest{
 			podName: pod.Name,
 			req:     req,
