c7cfb7fde7eef3a2888b1bc6ca276ac676b8fd0c
Felipe Oliveira <fpo@felipeweb.dev>
2020-08-21 22:31:07 -0300

    improve stream implementation
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- client/cluster/log.go
+++ client/cluster/log.go
@@ -5,13 +5,16 @@ Copyright 2020 Getup Cloud. All rights reserved.
 package cluster
 
 import (
+	"bufio"
 	"context"
+	"fmt"
 	"io"
 	"sync"
 	"time"
 
 	undistrov1 "github.com/getupcloud/undistro/api/v1alpha1"
 	"github.com/getupcloud/undistro/internal/scheme"
+	"github.com/getupcloud/undistro/log"
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/client-go/kubernetes"
@@ -38,6 +41,7 @@ func (l *logStreamer) Stream(ctx context.Context, c *rest.Config, w io.Writer, n
 		cancel context.CancelFunc
 	)
 	ctx, cancel = context.WithCancel(ctx)
+	c.Timeout = 0
 	l.kubeClient, err = client.New(c, client.Options{Scheme: scheme.Scheme})
 	if err != nil {
 		cancel()
@@ -92,7 +96,17 @@ func (l *logStreamer) streamLogs(ctx context.Context, reqs []logRequest, writer 
 		if err != nil {
 			return err
 		}
-		go l.readLog(ctx, reader, w)
+		pw := &prefixingWriter{
+			writer: w,
+			prefix: []byte(fmt.Sprintf("[%s] ", req.podName)),
+		}
+		go func(ctx context.Context, pod string) {
+			defer wg.Done()
+			err = l.readLog(ctx, reader, pw)
+			if err != nil {
+				log.Log.Error(err, "fail read log", "name", pod)
+			}
+		}(ctx, req.podName)
 	}
 	go func(ctx context.Context) {
 		wg.Wait()
@@ -102,9 +116,21 @@ func (l *logStreamer) streamLogs(ctx context.Context, reqs []logRequest, writer 
 	return err
 }
 
-func (l *logStreamer) readLog(ctx context.Context, reader io.ReadCloser, writer io.Writer) {
+func (l *logStreamer) readLog(ctx context.Context, reader io.ReadCloser, writer io.Writer) error {
 	defer reader.Close()
-	io.Copy(writer, reader)
+	r := bufio.NewReader(reader)
+	for {
+		bytes, err := r.ReadBytes('\n')
+		if _, err := writer.Write(bytes); err != nil {
+			return err
+		}
+		if err != nil {
+			if err != io.EOF {
+				return err
+			}
+			return nil
+		}
+	}
 }
 
 func (l *logStreamer) clusterIsReady(ctx context.Context, nm types.NamespacedName) (bool, error) {
@@ -141,4 +167,26 @@ func NewLogStreamer() *logStreamer {
 type logRequest struct {
 	podName string
 	req     *rest.Request
+}
+
+type prefixingWriter struct {
+	prefix []byte
+	writer io.Writer
+}
+
+func (pw *prefixingWriter) Write(p []byte) (int, error) {
+	if len(p) == 0 {
+		return 0, nil
+	}
+
+	// Perform an "atomic" write of a prefix and p to make sure that it doesn't interleave
+	// sub-line when used concurrently with io.PipeWrite.
+	n, err := pw.writer.Write(append(pw.prefix, p...))
+	if n > len(p) {
+		// To comply with the io.Writer interface requirements we must
+		// return a number of bytes written from p (0 <= n <= len(p)),
+		// so we are ignoring the length of the prefix here.
+		return len(p), err
+	}
+	return n, err
 }
