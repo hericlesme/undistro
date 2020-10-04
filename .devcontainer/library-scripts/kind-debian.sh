4515ab4f7a637a4018cef81b52e7b1967d5ea8e1
Felipe Oliveira <fpo@felipeweb.dev>
2020-10-03 21:19:08 -0300

    codespaces
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

---
+++ .devcontainer/library-scripts/kind-debian.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.9.0/kind-linux-amd64
+chmod +x ./kind
+mv ./kind /usr/local/bin/kind
---
+++ .devcontainer/library-scripts/tilt-debian.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+
+curl -fsSL https://raw.githubusercontent.com/tilt-dev/tilt/master/scripts/install.sh | bash
