b0de90431a7bd2e7a55534d23282d1064b9b39f6
Felipe Oliveira <fpo@felipeweb.dev>
2020-12-11 13:19:32 -0300

    validation
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- apis/app/v1alpha1/cluster_webhook.go
+++ apis/app/v1alpha1/cluster_webhook.go
@@ -17,7 +17,14 @@ limitations under the License.
 package v1alpha1
 
 import (
+	"fmt"
+	"reflect"
+
+	"github.com/getupio-undistro/undistro/pkg/version"
+	apierrors "k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/util/validation/field"
+	capi "sigs.k8s.io/cluster-api/api/v1alpha3"
 	ctrl "sigs.k8s.io/controller-runtime"
 	logf "sigs.k8s.io/controller-runtime/pkg/log"
 	"sigs.k8s.io/controller-runtime/pkg/webhook"
@@ -31,8 +38,6 @@ func (r *Cluster) SetupWebhookWithManager(mgr ctrl.Manager) error {
 		For(r).
 		Complete()
 }
-
-// EDIT THIS FILE!  THIS IS SCAFFOLDING FOR YOU TO OWN!
 
 // +kubebuilder:webhook:path=/mutate-app-undistro-io-v1alpha1-cluster,mutating=true,failurePolicy=fail,groups=app.undistro.io,resources=clusters,verbs=create;update,versions=v1alpha1,name=mcluster.undistro.io,sideEffects=None,admissionReviewVersions=v1beta1
 
@@ -53,26 +58,56 @@ func (r *Cluster) Default() {
 
 var _ webhook.Validator = &Cluster{}
 
+func (r *Cluster) validate(old *Cluster) error {
+	var allErrs field.ErrorList
+	if r.Spec.ControlPlane == nil && !r.Spec.InfrastructureProvider.Managed {
+		allErrs = append(allErrs, field.Required(
+			field.NewPath("spec", "controlPlane"),
+			"controlPlane must to be populated when is a self hosted cluster",
+		))
+	}
+	_, err := version.ParseVersion(r.Spec.KubernetesVersion)
+	if err != nil {
+		allErrs = append(allErrs, field.Invalid(
+			field.NewPath("spec", "kubernetesVersion"),
+			r.Spec.KubernetesVersion,
+			"kubernetesVersion must to be a semantic versioning",
+		))
+	}
+	if old != nil && r.Spec.ControlPlane != nil && !r.Spec.InfrastructureProvider.Managed {
+		if !reflect.DeepEqual(old.Spec.ControlPlane.Endpoint, capi.APIEndpoint{}) &&
+			!reflect.DeepEqual(r.Spec.ControlPlane.Endpoint, old.Spec.ControlPlane.Endpoint) {
+			allErrs = append(allErrs, field.Invalid(
+				field.NewPath("spec", "controlPlane", "endpoint"),
+				r.Spec.ControlPlane.Endpoint,
+				"field is immutable",
+			))
+		}
+	}
+	if len(allErrs) == 0 {
+		return nil
+	}
+	return apierrors.NewInvalid(GroupVersion.WithKind("Cluster").GroupKind(), r.Name, allErrs)
+}
+
 // ValidateCreate implements webhook.Validator so a webhook will be registered for the type
 func (r *Cluster) ValidateCreate() error {
 	clusterlog.Info("validate create", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object creation.
-	return nil
+	return r.validate(nil)
 }
 
 // ValidateUpdate implements webhook.Validator so a webhook will be registered for the type
 func (r *Cluster) ValidateUpdate(old runtime.Object) error {
 	clusterlog.Info("validate update", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object update.
-	return nil
+	oldCl, ok := old.(*Cluster)
+	if !ok {
+		return apierrors.NewBadRequest(fmt.Sprintf("expected a Cluster but got a %T", old))
+	}
+	return r.validate(oldCl)
 }
 
 // ValidateDelete implements webhook.Validator so a webhook will be registered for the type
 func (r *Cluster) ValidateDelete() error {
 	clusterlog.Info("validate delete", "name", r.Name)
-
-	// TODO(user): fill in your validation logic upon object deletion.
 	return nil
 }
