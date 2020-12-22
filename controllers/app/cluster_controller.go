cbdb70cfd4bbbbb32ec0556bd8bad934247ea146
Felipe Oliveira <fpo@felipeweb.dev>
2020-12-22 03:29:21 -0300

    eks
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- controllers/app/cluster_controller.go
+++ controllers/app/cluster_controller.go
@@ -27,7 +27,6 @@ import (
 	appv1alpha1 "github.com/getupio-undistro/undistro/apis/app/v1alpha1"
 	"github.com/getupio-undistro/undistro/pkg/kube"
 	"github.com/getupio-undistro/undistro/pkg/meta"
-	"github.com/getupio-undistro/undistro/pkg/predicate"
 	"github.com/getupio-undistro/undistro/pkg/retry"
 	"github.com/getupio-undistro/undistro/pkg/scheme"
 	"github.com/getupio-undistro/undistro/pkg/template"
@@ -44,7 +43,6 @@ import (
 	"sigs.k8s.io/cluster-api/util/kubeconfig"
 	"sigs.k8s.io/cluster-api/util/patch"
 	ctrl "sigs.k8s.io/controller-runtime"
-	"sigs.k8s.io/controller-runtime/pkg/builder"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/controller"
 	"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
@@ -374,7 +372,7 @@ func (r *ClusterReconciler) reconcileDelete(ctx context.Context, logger logr.Log
 
 func (r *ClusterReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
-		For(&appv1alpha1.Cluster{}, builder.WithPredicates(predicate.Changed{})).
+		For(&appv1alpha1.Cluster{}).
 		WithOptions(controller.Options{MaxConcurrentReconciles: 10}).
 		Complete(r)
 }
