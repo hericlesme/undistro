c3bc85e52fc8f5915ca795ecbe6b6d00aef49d0f
Felipe Oliveira <fpo@felipeweb.dev>
2021-06-29 20:29:11 -0300

    e2e: logs
    Signed-off-by: Felipe Oliveira <fpo@felipeweb.dev>

--- e2e/main_test.go
+++ e2e/main_test.go
@@ -122,6 +122,13 @@ func TestMain(m *testing.M) {
 		out, stderr, _ = cmd.Run(ctx)
 		fmt.Println(string(out))
 		fmt.Println("err:", string(stderr))
+		cmd = exec.NewCommand(
+			exec.WithCommand("kubectl"),
+			exec.WithArgs("describe", "pods", "-n", "undistro-system"),
+		)
+		out, stderr, _ = cmd.Run(ctx)
+		fmt.Println(string(out))
+		fmt.Println("err:", string(stderr))
 		os.Exit(1)
 	}
 	config, err := clientcmd.BuildConfigFromFlags("", filepath.Join(homedir.HomeDir(), ".kube", "config"))
